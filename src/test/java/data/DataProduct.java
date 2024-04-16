02e7cc8b1d93678b1e02d26a78cfa6a02149bf6a
Dinar <dinar-97@mail.ru>
2024-04-16 16:03:52 +0300

    added new tests with allure

--- src/test/java/data/DataProduct.java
+++ src/test/java/data/DataProduct.java
@@ -8,7 +8,12 @@ public class DataProduct {
     private final String productName1 = "Капуста";
     private final String productName2 = "Помидор";
     private final String productName3 = "Яблоко";
-    private final String productName4 = "Банан";
+
+    private final String productExoticFruit = "Манго";
+    private final String productFruit = "Банан";
+    private final String productExoticVegetable = "Кивано";
+    private final String productVegetable = "Картофель";
+    private final String emptyField = "";
 
     private final String productTypeFruit = "FRUIT";
     private final String productTypeVeg = "VEGETABLE";
