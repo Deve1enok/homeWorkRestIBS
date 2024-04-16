047d6f3a5634fad20387dfc4e6c9f817c7de1a05
Dinar <dinar-97@mail.ru>
2024-04-16 12:32:09 +0300

    added allure api tests

--- src/test/java/tests/AddingProductTest.java
+++ src/test/java/tests/AddingProductTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import data.DataProduct;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
@@ -11,15 +13,20 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static specs.ProductSpecification.requestSpecification;
 import static specs.ProductSpecification.responseSpecification200;
 
+@DisplayName("QualitApi")
 public class AddingProductTest {
     DataProduct dataProduct = new DataProduct();
     ProductListModel productListModel = new ProductListModel();
+    private String cookies;
 
     @Test
+    @Owner("Fazlyakhemtov D.A.")
+    @Feature("Добавление/сброс товара")
     @DisplayName("Проверка добавления/сброса фрукта с типом фрукт и чек-боксом экзотический")
     public void addingNewProductAndCheckThemForAdding() {
         RestAssured.defaultParser = Parser.JSON;
@@ -28,70 +35,91 @@ public class AddingProductTest {
         productListModel.setType(dataProduct.getProductTypeFruit());
         productListModel.setExotic(true);
 
+        Response response = step("Отправляем Post запрос на добавление товара", () ->
+                given(requestSpecification)
 
-        Response response = given(requestSpecification)
+                        .body(productListModel)
 
-                .body(productListModel)
+                        .when()
+                        .post("/food")
 
-                .when()
-                .post("/food");
+                        .then()
+                        .spec(responseSpecification200)
+                        .extract().response());
 
-        response.then()
-                .spec(responseSpecification200)
-                .extract().response().getDetailedCookies();
+        cookies = response.getHeader("Set-Cookie");
 
-        String cookies = response.getHeader("Set-Cookie");
+        List<ProductListModel> products = step("Отправляем Get запрос на получение добавленного товара", () ->
+                given(requestSpecification)
 
-        List<ProductListModel> products = given(requestSpecification)
+                        .cookie(cookies)
+                        .when()
+                        .get("/food")
 
-                .cookie(cookies)
-                .when()
-                .get("/food")
+                        .then()
+                        .spec(responseSpecification200)
+                        .extract().body().jsonPath().getList("", ProductListModel.class));
 
-                .then()
-                .spec(responseSpecification200)
-                .extract().body().jsonPath().getList("", ProductListModel.class);
+        step("Проверяем тело-ответа", () ->
+                Assertions.assertAll(
+                        () -> Assertions.assertEquals(dataProduct.getProductName4(), products.get(4).getName()),
+                        () -> Assertions.assertEquals(dataProduct.getProductTypeFruit(), products.get(4).getType()),
+                        () -> Assertions.assertTrue(products.get(4).isExotic())
+                ));
 
+        step("Отправляем Post запрос для сброса данных", () ->
+                given(requestSpecification)
 
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(dataProduct.getProductName4(), products.get(4).getName()),
-                () -> Assertions.assertEquals(dataProduct.getProductTypeFruit(), products.get(4).getType()),
-                () -> Assertions.assertTrue(products.get(4).isExotic())
-        );
+                        .cookie(cookies)
+                        .when()
+                        .post("/data/reset")
 
-        Response responsee = given(requestSpecification)
+                        .then()
 
-                .when()
-                .post("/data/reset");
+                        .spec(responseSpecification200)
+                        .extract().response());
 
-        responsee.then()
+        List<ProductListModel> productsAfterReset = step(
+                "Отправляем Get запрос на получение списка после сброса данных", () ->
+                        given(requestSpecification)
 
-                .spec(responseSpecification200)
-                .extract().response();
+                                .cookie(cookies)
+                                .when()
+                                .get("/food")
 
-        List<ProductListModel> productsAfterReset = given(requestSpecification)
+                                .then()
+                                .spec(responseSpecification200)
+                                .extract().body().jsonPath().getList("", ProductListModel.class));
 
-                .when()
-                .get("/food")
+        step("Проверяем тело-ответа", () ->
+                Assertions.assertAll(
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductName(), productsAfterReset.get(0).getName()),
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductTypeFruit(), productsAfterReset.get(0).getType()),
+                        () -> Assertions.
+                                assertTrue(productsAfterReset.get(0).isExotic()),
 
-                .then()
-                .spec(responseSpecification200)
-                .extract().body().jsonPath().getList("", ProductListModel.class);
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductName1(), productsAfterReset.get(1).getName()),
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductTypeVeg(), productsAfterReset.get(1).getType()),
+                        () -> Assertions.
+                                assertFalse(productsAfterReset.get(1).isExotic()),
 
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductName2(), productsAfterReset.get(2).getName()),
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductTypeVeg(), productsAfterReset.get(2).getType()),
+                        () -> Assertions.
+                                assertFalse(productsAfterReset.get(2).isExotic()),
 
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(dataProduct.getProductName(), productsAfterReset.get(0).getName()),
-                () -> Assertions.assertEquals(dataProduct.getProductTypeFruit(), productsAfterReset.get(0).getType()),
-                () -> Assertions.assertTrue(productsAfterReset.get(0).isExotic()),
-                () -> Assertions.assertEquals(dataProduct.getProductName1(), productsAfterReset.get(1).getName()),
-                () -> Assertions.assertEquals(dataProduct.getProductTypeVeg(), productsAfterReset.get(1).getType()),
-                () -> Assertions.assertFalse(productsAfterReset.get(1).isExotic()),
-                () -> Assertions.assertEquals(dataProduct.getProductName2(), productsAfterReset.get(2).getName()),
-                () -> Assertions.assertEquals(dataProduct.getProductTypeVeg(), productsAfterReset.get(2).getType()),
-                () -> Assertions.assertFalse(productsAfterReset.get(2).isExotic()),
-                () -> Assertions.assertEquals(dataProduct.getProductName3(), productsAfterReset.get(3).getName()),
-                () -> Assertions.assertEquals(dataProduct.getProductTypeFruit(), productsAfterReset.get(3).getType()),
-                () -> Assertions.assertFalse(productsAfterReset.get(3).isExotic())
-        );
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductName3(), productsAfterReset.get(3).getName()),
+                        () -> Assertions.
+                                assertEquals(dataProduct.getProductTypeFruit(), productsAfterReset.get(3).getType()),
+                        () -> Assertions.
+                                assertFalse(productsAfterReset.get(3).isExotic())
+                ));
     }
 }
